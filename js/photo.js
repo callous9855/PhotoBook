3ad9e888321f2148ffe5400af3e95bdc694931fe
Callous Nguyen <nguyenmanhhung9b@gmail.com>
2018-05-16 00:08:34 +0700

    show image function

--- js/photo.js
+++ js/photo.js
@@ -22,3 +22,25 @@
   window.addEventListener('scroll', updateTimelineItems);
   updateTimelineItems(null, true);
 })();
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+    document.getElementById("photo-timeline").style.display = 'none';
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+  document.getElementById("photo-timeline").style.display = ""; 
+}
