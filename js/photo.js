3a2117e28e44992892e5ccdc9e72ff787e03618b
sinbadkhang <singbad1998@gmail.com>
2018-05-16 18:42:50 +0700

    temp ajax and function in photo.js

--- js/photo.js
+++ js/photo.js
@@ -153,11 +153,94 @@ function uploadFile(file, i) {
 }
 // end drag and drop image
 
+// GET DATA
+// get group photo story
+function getGroupPhoto(){
+
+}
+
+// get group list
+function getGroupList(){
+
+}
 
 // start document
 $(document).ready(function(){
+  getGroupPhoto();
+  getGroupList();
 
-  // start upload photo story
+  //3.1 start add group
+  $('#').click(function (e){
+    e.preventDefault();
+    var form = $('#').serialize();
+
+    $.ajax({
+      url: '/groups/create',
+      method: 'POST',
+      dataType: 'json',
+      data: form,
+
+    }).done(function (data){
+      
+      // reload
+      // getGroupList();
+
+    }).fail(function (error, group) {
+      console.log(error.message); 
+    
+      // json group object 
+      console.log(group);
+    });
+  }
+  // end add group
+
+  //3.2.1 start search user
+  $('#us_find_btn').click(function (e){
+    e.preventDefault();
+    var form = $('#').serialize();
+
+    $.ajax({
+      url: '/findUser',
+      method: 'POST',
+      dataType: 'json',
+      data: form,
+
+    }).done(function (data){
+      
+
+    }).fail(function (error, username, found) {
+      console.log(error.message); 
+      console.log(found);
+      // json username object 
+      console.log(username);
+    });
+  }
+  // end search user
+
+  //3.2.2 start add user
+  $('#us_add_btn').click(function (e){
+    e.preventDefault();
+    var form = $('#').serialize();
+
+    $.ajax({
+      url: '/groups/addMember',
+      method: 'POST',
+      dataType: 'json',
+      data: form,
+
+    }).done(function (data){
+      
+
+    }).fail(function (error, member) {
+      console.log(error.message); 
+    
+      // json photo object 
+      console.log(member);
+    });
+  }
+  // end add user
+
+  //4.1 start upload photo story
   $('#up_photo_btn').click(function (e){
     e.preventDefault();
     var form = document.querySelector('#up_photo_form');
@@ -173,8 +256,9 @@ $(document).ready(function(){
     }).done(function (data){
       form.reset();
 
+      // reload
       // getGroupPhoto();
-      
+
     }).fail(function (error, photo) {
       console.log(error.message); 
       // json photo object 
