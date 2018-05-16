625c55acf3649d1b3337b07d697468e63df6c2a4
sinbadkhang <singbad1998@gmail.com>
2018-05-16 08:55:02 +0700

    signup_password_and_repassword

--- js/login.js
+++ js/login.js
@@ -5,3 +5,17 @@ $('.toggle').on('click', function() {
 $('.close').on('click', function() {
   $('.container').stop().removeClass('active');
 });
+
+// sign up password checking
+$('#su_form').one('submit', function(e) {
+    e.preventDefault();
+    
+    var password = $('#su_password').val();
+	var repassword = $('#su_repassword').val();
+
+	if (password === repassword) {
+		$(this).submit();
+	}else {
+		alert('Password and Repassword do not match');
+	}
+});
