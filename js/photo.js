82bc29f2ca23d93dd29db05801af22fde7092e25
Callous Nguyen <nguyenmanhhung9b@gmail.com>
2018-05-16 08:51:46 +0700

    add dropdown show group

--- js/photo.js
+++ js/photo.js
@@ -24,23 +24,26 @@
 })();
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
+// Start zoom picture
+  // Get the modal
+  var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-img.onclick = function(){
-    document.getElementById("photo-timeline").style.display = 'none';
-    modal.style.display = "block";
-    modalImg.src = this.src;
-}
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById('myImg');
+  var modalImg = document.getElementById("img01");
+  img.onclick = function(){
+      document.getElementById("photo-timeline").style.display = 'none';
+      modal.style.display = "block";
+      modalImg.src = this.src;
+  }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-  document.getElementById("photo-timeline").style.display = ""; 
-}
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+    document.getElementById("photo-timeline").style.display = ""; 
+  }
+// End zoom picture
+
