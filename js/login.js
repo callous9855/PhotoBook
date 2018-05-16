f3e5514782c2702fd912ebebbeeedd9949cdfa49
sinbadkhang <singbad1998@gmail.com>
2018-05-16 17:33:41 +0700

    fix retype password check and special characters

--- js/login.js
+++ js/login.js
@@ -6,16 +6,24 @@ $('.close').on('click', function() {
   $('.container').stop().removeClass('active');
 });
 
-// sign up password checking
-$('#su_form').one('submit', function(e) {
-    e.preventDefault();
-    
-    var password = $('#su_password').val();
+// sign up passwords checking
+$('#su_form').submit(function(e){
+	var password = $('#su_password').val();
 	var repassword = $('#su_repassword').val();
 
 	if (password === repassword) {
-		$(this).submit();
-	}else {
-		alert('Password and Repassword do not match');
-	}
+    	// if matches then form will submit
+    	return;
+  	}
+  	else {
+  		// if not then show title
+  		$('#su_repassword').attr('title', 'Not matches');
+  		// console.log(password +"\n"+ repassword);
+  	}
+	
+  	e.preventDefault();
 });
+
+// prevent special characters
+$('input').attr('pattern', '[a-zA-Z0-9]{6,}');
+$('input').attr('title', 'Cannot contain special characters and must be at least 6 characters');
